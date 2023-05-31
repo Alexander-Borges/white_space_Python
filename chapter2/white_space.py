0d9230cde36fb16232d8536b6e06293dae6ef298
Alexander Borges <borges2721@gmail.com>
2023-05-31 10:43:40 -0400

    first commit

---
+++ chapter2/white_space.py
@@ -0,0 +1,16 @@
+print("Python")
+print("\tPython")
+
+print("Languages:\nPython\nC\nJavaScript") 
+
+favorite_language = 'python '
+print(favorite_language) 
+favorite_language.rstrip()
+print(favorite_language)
+favorite_language = favorite_language.rstrip()
+print(favorite_language)
+
+programming_language = ' python '
+programming_language.strip()
+programming_language = programming_language.strip()
+print(programming_language)
